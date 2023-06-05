294cc3cb263b2e71faa4b6fe4f2b8d9af47f208d
Parashkar prajwal <prajwalparashkar100@gmail.com>
2023-06-05 23:12:52 +0530

    email and search methods in contact controller added

--- Controllers/ContactController.js
+++ Controllers/ContactController.js
@@ -12,12 +12,40 @@ const asyncHandler = require("express-async-handler");
 const Contact = require("../Models/ContactModel")
 const constants = require("../routes/Constants");
 
-//wrap the function is this asyncHandler
+//wrap the function in this asyncHandler
 //this will do automatic try catch exception handling
 const getContacts =asyncHandler(async (req,res)=>{
     const contacts =await Contact.find({user_id:req.user.id});
     res.status(200).json(contacts)
 })
+
+const seachByNumber = asyncHandler(async(req,res)=>{
+    const{phone} = req.body;
+    console.log(phone);
+    const contact =await Contact.findOne({phone})
+    console.log(contact);
+    if(!contact){
+        res.status(400).send({message:`Contact with ${phone} not found`})
+    }
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(400).send({message :`The current user does not have access to this contact`})
+    }
+    res.status(200).json(contact);    
+})
+
+const searchByEmail = asyncHandler(async (req, res)=>{
+    const{email} = req.body;
+    console.log(email)
+    const contact =await Contact.findOne({email})
+    if(!contact){
+        res.status(400).send({message:`Contact with ${email} not found`})
+    }
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(400).send({message :`The current user does not have access to this contact`})
+    }
+    res.status(200).json(contact);  
+})
+
 
 //desc for get single contact
 //route /api/contacts/:id
@@ -80,10 +108,35 @@ const updateContact =asyncHandler(async (req,res)=>{
     res.status(200).json(updatedContact)
 })
 
+//desc  search by name of contact
+//route /api/contacts/name
+//access will be private
+
+// const searchByName=asyncHandler(async (req, res)=>{
+   
+//     const {name} = req.body;
+//     // console.log(name)
+    
+//     const contact = await Contact.findOne({name: name})//req.params.name
+   
+//     console.log(contact)
+//     if(!contact){
+//         console.log("first if block")
+//         res.status(400).send({message:"Contact not found"})
+//     }
+//     if(contact.user._id.toString() !== req.user.id){
+//         console.log("second if block")
+//         res.status(403).send({message:"User don't have permission to access this contacts"});
+//     }
+    
+//     res.status(200).json(contact);
+// })
+
 //desc for delete single contact
 //route /api/contacts/:id
 //for now access will be private
 const deleteContact =asyncHandler( async(req,res)=>{
+    console.log("the delete method")
     const contact =await Contact.findById(req.params.id)
     if(!contact){
         res.status(404);
@@ -91,11 +144,24 @@ const deleteContact =asyncHandler( async(req,res)=>{
     }
     if(contact.user_id.toString() !== req.user.id){
         res.status(403)
-        throw new Error("USer don't have permission to delete this contacts");
+        throw new Error("User don't have permission to delete this contacts");
     }   
     await Contact.deleteOne({_id:req.params.id})
-    res.status(200).json({message:`Delete contact with ${req.params.id} id`})
+    res.status(200).json({message:`Delete contact with ${contact.name} id`})
 })
 
 
-module.exports={getContacts,getContact,createContact,updateContact,deleteContact}
+
+
+
+
+module.exports={
+    getContacts,
+    getContact,
+    createContact,
+    updateContact,
+    deleteContact,
+    // searchByName,
+    seachByNumber,
+    searchByEmail
+    }
--- routes/ContactRoute.js
+++ routes/ContactRoute.js
@@ -1,17 +1,27 @@
 const express = require("express")
 const router = express.Router()
-const {getContacts,getContact,createContact,updateContact,deleteContact} = require("../Controllers/ContactController")
+const {searchByName,
+    getContacts,
+    getContact,
+    createContact,
+    updateContact,
+    deleteContact,
+    seachByNumber,
+    searchByEmail
+    } = require("../Controllers/ContactController")
 const validateToken = require("../Controllers/Middleware/validateTokenHandler")
 
 
 //if you have all the routes as private routes then we can add following line 
-//It is a bit hard to read routing
+//It is a bit hard to read this style routing
 router.use(validateToken)
 router.route("/").get(getContacts).post(createContact)
+// router.route("/name").post(searchByName)
 router.route("/:id").get(getContact).delete(deleteContact).put(updateContact)
+router.route("/phone").post(seachByNumber);
+router.route("/email").post(searchByEmail)
+//we can alos write the routing as above to save space
 
-
-//we can alos write the routing as above to save space 
 
 //router.route("/").get(getContacts)
 // router.route("/").post(createContact)

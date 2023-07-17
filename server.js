37b1a20765a44b9c5e7fce0538764c0efecb73c4
Parashkar prajwal <prajwalparashkar100@gmail.com>
2023-07-17 12:16:37 +0530

    installed cors and added access to all the coross origins i.e. given public access to the api

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+var cors = require('cors')
 const Connectdb = require("./Config/dbConnection")
 const { errorHandler } = require('./Controllers/Middleware/ErrorHandler');
 const app=express();
-const port=process.env.PORT ||3000 
+const port=process.env.PORT ||3001 
 const dotenv = require('dotenv').config();
-
+app.use(cors())
 Connectdb();
+
 //to parse all data objects in json we need to use a middleware provided by express
 app.use(express.json())
 app.use("/api/contacts", require("./routes/ContactRoute"))
